49a5c06b1a0d61c2b907f9f1cd0eaf73643cd6fb
HuseyinAkkoc <156607663+HuseyinAkkoc@users.noreply.github.com>
2024-09-23 13:43:57 -0400

    Added some extras.

--- Assets/Scripts/AxesDraw.cs
+++ Assets/Scripts/AxesDraw.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class AxesDraw : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    public float lenght = 1.0f;
+    public bool local = true;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 start= transform.position;
+        Vector3 right= local ?transform.right: Vector3.right;
+        Vector3 up= local ?transform.up :Vector3.up;
+        Vector3 forward= local ?transform.forward: Vector3.forward;
+        Debug.DrawLine(start, start+ right*lenght,Color.red);
+        Debug.DrawLine(start, start +up * lenght, Color.green);
+        Debug.DrawLine(start, start + forward * lenght, Color.blue);
     }
 }
