36169d3fc0666e50edda9215bed9573deb00dd13
HuseyinAkkoc <156607663+HuseyinAkkoc@users.noreply.github.com>
2024-09-24 06:43:16 -0400

    Lab1/Homework1 is completed! Motion on x and y axes added.

--- Assets/Scripts/FunkyMotion.cs
+++ Assets/Scripts/FunkyMotion.cs
@@ -13,10 +13,15 @@ public class FunkyMotion : MonoBehaviour
     void FixedUpdate()
     {
 
-        float z= Mathf.Sin(time);   
+     
 
         float dt= Time.fixedDeltaTime;
 
+        float z = 0.0f;   //Example of frecuency & amplitude // Mathf.Sin(time* 10.0f)*5.0f;   
+
+
+        x= x+ (-Mathf.Sin(time*frequency)* frequency*amplitude*dt);
+        y=y + (-Mathf.Cos(time * frequency) * frequency * amplitude * dt);
         transform.position = new Vector3(x,y,z);
 
         time += dt;
